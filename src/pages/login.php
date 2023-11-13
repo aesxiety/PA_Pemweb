2c1a4878678e821bd11d46448388592e57324a6a
Ayruszy <145456856+Ayruszy@users.noreply.github.com>
2023-11-13 19:39:45 +0800

    Update login.php

--- src/pages/login.php
+++ src/pages/login.php
@@ -44,10 +44,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../asset/logo.png">
+    <link rel="stylesheet" href="../style/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Ngawi Sole</title>
 </head>
 <body>
-    <div class="container-form">
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="input">
+        <h1>Log In Cuy!</h1>
+        <?php
+    if (isset($error)) {
+    } else {
+        echo "<p style='color: white; display:none;'> Username/Password Salah! </p>";
+    }
+    ?>
+        <form action="" method="post">
+            <div class="container-form">
+                <i class="fa fa-envelope-open-text"></i> 
+                <input type="text" placeholder="Username" name="username" id="input" required>
+            </div>
+            <div class="container-form">
+                <i class="fa fa-lock"></i>
+                <input type="password" placeholder="Password" name="password" id="input" required>
+            </div>
+            <button type="submit" name="login" class="btn-input">Log In</button>
+            <div class="links">
+                Don't have account? <a href="../pages/regis.php">Sign Up Now</a>
+            </div>
+        </form>
+        <!-- <div class="container-form">
         <form action="" method="post">
             <label for="" class="left">Username : </label>
             <input type="text" name="username" required><br>
@@ -59,6 +85,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 Don't have account? <a href="../pages/regis.php">Sign Up Now</a>
             </div>
         </form>
-    </div>
+    </div> -->
 </body>
 </html>
