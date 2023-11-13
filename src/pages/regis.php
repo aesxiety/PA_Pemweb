106757fc769bbbdb97190bdd051b9ba5d937cc48
Ayruszy <145456856+Ayruszy@users.noreply.github.com>
2023-11-13 19:47:23 +0800

    Update regis.php

--- src/pages/regis.php
+++ src/pages/regis.php
@@ -36,6 +36,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../asset/logo.png">
+    <link rel="stylesheet" href="../style/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Ngawi Sole</title>
 </head>
 <body>
